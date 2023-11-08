d29aadf30fb1824c38f93db4df3b5eafe8b7f7ba
Supraj Gijre <suprajgijre@gmail.com>
2023-11-08 17:09:25 +0530

    01 and 02 (BASICS)

---
+++ 01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 133442
+let accountEmail = "gij@kmfs.com"
+var accountPswd = "bla"
+accountCity = 'bla'
+
+//accountId = 2 NOT ALLOWED TO CHANGE const variables
+
+
+/*
+We dont use var to declare variables as it is a bit buggy and value of var variables are faulty due to block scope and function scope
+*/
+
+
+console.table([accountEmail,accountId,accountPswd,accountCity]);
---
+++ 02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; // this treats all JS code as newer version
+
+// alert(3+3) wont work on Nodejs obv
+
+// number => 2 to power 53
+//  bigint
+//  string => ""
+//  boolean => true/false
+//  null => standalone value
+//  undefined =>
+//  symbol => unique
+
+console. log(typeof undefined); // undefined
+console. log(typeof null); // object
